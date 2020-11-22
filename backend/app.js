53de11737b97ecb35c47022d199761c1742e022d
Daniel Galvis <dangaltor@gmail.com>
2020-11-21 20:48:41 -0500

    Mongoose set up

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const db = require('./config/mongoose');
 
 var app = express();
 
@@ -13,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
