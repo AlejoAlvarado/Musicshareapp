17ecf7b018dcd82df907fd07ceac274ad90a3be6
Alejandro Alvarado <Nuestrocanal1@gmail.com>
2020-11-21 22:42:56 -0500

    Starting usersSchema in backend

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,30 @@
+const User = require("../models/users");
+
+exports.index = function (req, res, next) {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.send(users);
+    }
+  });
+};
+
+exports.create = function (req, res, next) {
+  let user = new User({
+    name: req.body.name,
+    username: req.body.username,
+    userid: req.body.userid,
+    password: req.body.password,
+    photo: req.body.photo,
+    state: req.body.state,
+  });
+
+  user.save((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.send("User guardado exitosamente");
+    }
+  });
+};
