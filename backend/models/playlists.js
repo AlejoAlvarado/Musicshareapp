a8b9fd386749f9f184405cacf19d6df427b47a76
Daniel Galvis <dangaltor@gmail.com>
2020-11-22 15:49:25 -0500

    Added reference to owner for playlist model

--- backend/models/playlists.js
+++ backend/models/playlists.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User = require('./users');
 
 let PlaylistSchema = Schema({
     title: {type: String, required: true, max:100},
-    cover: {type: String, required: false}
-})
+    cover: {type: String, required: false},
+    owner: {type: Schema.Types.ObjectId, ref: "User", required: true}
+});
+
+module.exports = mongoose.model('Playlist', PlaylistSchema);
