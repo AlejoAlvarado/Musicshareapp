17385f1d4bcf0600d972613c35da6a3e2967d35c
Alejandro Alvarado <Nuestrocanal1@gmail.com>
2020-11-24 17:17:01 -0500

    added signup component

---
+++ frontend/src/components/UserViews/Signup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-container>
+      <div>
+        <v-form ref="form">
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="user.name"
+                :rules="nameRules"
+                label="Nombre"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="user.lastname"
+                :rules="nameRules"
+                label="Apellido"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,23 @@ import vuetify from "@/plugins/vuetify"; // path to vuetify export
 import VueRouter from "vue-router";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import Signup from "./components/UserViews/Signup";
+
+const routes = [
+  {
+    path: "/signup",
+    component: Signup,
+  },
+];
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
-
 Vue.use(VueAxios, axios);
+
+const router = new VueRouter({ routes });
 
 new Vue({
   vuetify,
+  router,
   render: (h) => h(App),
 }).$mount("#app");
