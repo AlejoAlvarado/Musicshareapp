634532c1c21801e574da133922348b5ebb959120
Daniel Galvis <dangaltor@gmail.com>
2020-11-26 19:51:33 -0500

    Separate playlist card on a different Component

---
+++ frontend/src/components/PlaylistViews/PlaylistCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card dark>
+    <div class="d-flex flex-no-wrap justify-space-between">
+      <div>
+        <v-card-title class="headline" v-text="playlist.title"></v-card-title>
+
+        <v-card-subtitle v-text="'By: ' + playlist.owner"></v-card-subtitle>
+
+        <v-card-actions>
+          <v-btn class="ml-2 mt-5" outlined rounded small>
+            OPEN PLAYLIST
+          </v-btn>
+        </v-card-actions>
+      </div>
+
+      <v-avatar class="ma-3" size="125" tile>
+        <v-img :src="playlist.cover"></v-img>
+      </v-avatar>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    playlist: {
+      type: Object,
+    },
+  },
+};
+</script>
--- frontend/src/components/PlaylistViews/Playlists.vue
+++ frontend/src/components/PlaylistViews/Playlists.vue
@@ -2,25 +2,7 @@
   <v-container>
     <v-row dense>
       <v-col v-for="(playlist, i) in playlists" :key="i" cols="12" md="6" lg="4">
-        <v-card dark>
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title class="headline" v-text="playlist.title"></v-card-title>
-
-              <v-card-subtitle v-text="'By: ' + playlist.owner"></v-card-subtitle>
-
-              <v-card-actions>
-                <v-btn class="ml-2 mt-5" outlined rounded small>
-                  OPEN PLAYLIST
-                </v-btn>
-              </v-card-actions>
-            </div>
-
-            <v-avatar class="ma-3" size="125" tile>
-              <v-img :src="playlist.cover"></v-img>
-            </v-avatar>
-          </div>
-        </v-card>
+        <playlist-card :playlist="playlist" />
       </v-col>
     </v-row>
   </v-container>
@@ -28,8 +10,10 @@
 
 <script>
 import axios from '../../config/axios';
+import PlaylistCard from './PlaylistCard.vue';
 
 export default {
+  components: { PlaylistCard },
   data: () => ({
     playlists: [],
   }),
