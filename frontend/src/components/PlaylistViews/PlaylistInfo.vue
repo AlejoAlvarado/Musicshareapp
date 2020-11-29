404bc82505e5ce55ea5276dc4fa1a17780f04ff3
Daniel Galvis <dangaltor@gmail.com>
2020-11-28 22:09:56 -0500

    Show songs of playlists (Songs are code-burn)

--- frontend/src/components/PlaylistViews/PlaylistInfo.vue
+++ frontend/src/components/PlaylistViews/PlaylistInfo.vue
@@ -5,7 +5,7 @@
         <playlist-card :playlist="playlist" />
       </v-col>
     </v-row>
-    <song-list />
+    <song-list :songs="songs"/>
   </div>
 </template>
 <script>
@@ -17,6 +17,32 @@ export default {
   components: { PlaylistCard, SongList },
   data: () => ({
     playlist: {},
+    songs: [
+            {
+                title: "Service Bell",
+                artist: "Daniel Simion",
+                url: "https://soundbible.com/mp3/service-bell_daniel_simion.mp3",
+                image: "https://source.unsplash.com/crs2vlkSe98/400x400"
+            },
+            {
+                title: "Meadowlark",
+                artist: "Daniel Simion",
+                url: "https://soundbible.com/mp3/meadowlark_daniel-simion.mp3",
+                image: "https://source.unsplash.com/35bE_njbG9E/400x400"
+            },
+            {
+                title: "Hyena Laughing",
+                artist: "Daniel Simion",
+                url: "https://soundbible.com/mp3/hyena-laugh_daniel-simion.mp3",
+                image: "https://source.unsplash.com/Esax9RaEl2I/400x400"
+            },
+            {
+                title: "Creepy Background",
+                artist: "Daniel Simion",
+                url: "http://soundbible.com/mp3/creepy-background-daniel_simon.mp3",
+                image: "https://source.unsplash.com/j0g8taxHZa0/400x400"
+            }
+        ]
   }),
   created() {
     const playlistId = this.$route.params.id;
--- frontend/src/components/PlaylistViews/SongsList.vue
+++ frontend/src/components/PlaylistViews/SongsList.vue
@@ -1,33 +1,31 @@
 <template>
   <v-simple-table>
-      <thead>
-          <tr>
-              <th>
-                  Cover
-              </th>
-              <th>
-                  Title
-              </th>
-              <th>
-                  Artist
-              </th>
-          </tr>
-      </thead>
-      <tbody>
-          <tr v-for="(song, i) in songs" :key="i">
-              <td><v-img :src="song.cover"></v-img></td>
-          </tr>
-      </tbody>
+    <thead>
+      <tr>
+        <th>Cover</th>
+        <th>Title</th>
+        <th>Artist</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(song, i) in songs" :key="i">
+        <td>
+          <v-img :src="song.image" max-height="100" max-width="100"></v-img>
+        </td>
+        <td>{{ song.title }}</td>
+        <td>{{ song.artist }}</td>
+      </tr>
+    </tbody>
   </v-simple-table>
 </template>
 
 <script>
 export default {
-    props:{
-        songs:{
-            type: Array,
-        }
-    }
-
-}
+  props: {
+    songs: {
+      type: Array,
+    },
+  },
+};
 </script>
+
