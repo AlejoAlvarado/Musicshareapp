0fab6fbb1c2c4efaac0d25ef2918b8cbf31ff6a6
Alejandro Alvarado <Nuestrocanal1@gmail.com>
2020-12-01 19:04:52 -0500

    Added validation for signup and signin in backend

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -33,24 +33,33 @@ exports.index = function (req, res, next) {
 };
 
 exports.create = function (req, res, next) {
-  let user = new User({
-    name: req.body.name,
-    username: req.body.username,
-    //userid: req.body.userid,
-    password: bcrypt.hashSync(req.body.password, 8),
-    //photo: req.body.photo,
-    //state: req.body.state,
-    email: req.body.email,
-  });
-
-  user.save((err) => {
-    if (err) {
-      res
-        .status(500)
-        .send({ message: `Error al crear el usuario: ${err}`, message2: err });
+  User.find({ email: req.body.email }, (err, users) => {
+    if (users.length > 0) {
+      return res
+        .status(400)
+        .send({ message: "Un usuario con este email ya existe" });
     } else {
-      return res.status(200).send({
-        message: "User guardado exitosamente",
+      let user = new User({
+        name: req.body.name,
+        username: req.body.username,
+        //userid: req.body.userid,
+        password: bcrypt.hashSync(req.body.password, 8),
+        //photo: req.body.photo,
+        //state: req.body.state,
+        email: req.body.email,
+      });
+
+      user.save((err) => {
+        if (err) {
+          res.status(500).send({
+            message: `Error al crear el usuario: ${err}`,
+            message2: err,
+          });
+        } else {
+          return res.status(200).send({
+            message: "User guardado exitosamente",
+          });
+        }
       });
     }
   });
@@ -61,11 +70,19 @@ exports.signinUser = function (req, res, next) {
     if (err) return req.status(500).send({ message: err });
     if (!user) return req.status(404).send({ message: "User no encontrado" });
 
-    req.user = user;
-    res.status(200).send({
-      message: "User logeado exitosamente!",
-      token: service.createToken(user),
-    });
+    console.log(user[0].password);
+    let equalPass = bcrypt.compareSync(req.body.password, user[0].password);
+    console.log(equalPass);
+    if (equalPass) {
+      req.user = user;
+      return res.status(200).send({
+        message: "User logeado exitosamente!",
+        token: service.createToken(user),
+      });
+    } else {
+      console.log("mala contraseña");
+      return res.status(400).send({ message: "La contraseña es incorrecta" });
+    }
   });
 };
 
