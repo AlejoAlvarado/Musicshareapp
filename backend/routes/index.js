8cef60b246d205c98c3508daa678424a6faa616a
Alejandro Alvarado <Nuestrocanal1@gmail.com>
2020-12-05 20:40:19 -0500

    Added auth middleware for roles

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const auth = require("../middlewares/auth");
+const { isAuth } = require("../middlewares/auth");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
-router.get("/private", auth, (req, res) => {
+router.get("/private", isAuth, (req, res) => {
   console.log("res status:" + req.status);
   res.status(200).send({ message: "Tienes acceso" });
 });
